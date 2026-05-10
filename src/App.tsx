c5a35a153be5f8d8753a1b786582b278524a5d3b
tanvirhossainapon <tanvirhossainapon@gmail.com>
2026-05-10 17:47:54 +0600

    chore: shadcn ui has been added

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className="p-10">
+
+        <h1 className="text-3xl font-bold underline">
+          Hello auth front end !
+        </h1>
+        <Button> Click Me</Button>
+     </div>
+     
+    </>
+  )
+}
+
+export default App
